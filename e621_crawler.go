67bc2707b404ef6d963ac513a831b6b21bbef65b
eiffelwong1 <eiffelynx@gmail.com>
2020-05-03 23:02:03 -0700

    downloades but sometime can't avoid server 500 error yet

--- e621_crawler.go
+++ e621_crawler.go
@@ -7,13 +7,14 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
 )
 
 var homeURL string = "https://www.e621.net/posts.json"
-var e621RateLimiter = time.Tick(501 * time.Millisecond)
+var e621RateLimiter = time.Tick(1010 * time.Millisecond)
 
 var missingPID = []int{}
 
@@ -31,9 +32,16 @@ type e621PostList struct {
 
 func safeGet(URL string) *http.Response {
 	// !! the caller are responsible to call "defer response.Body.Close()" for clean up
+	<-e621RateLimiter
 	response, err := http.Get(URL)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("ERROR: " + strconv.Itoa(response.StatusCode) + " when getting: " + URL)
+		response.Body.Close()
+		if response.StatusCode >= 500 && response.StatusCode < 600 {
+			time.Sleep(5 * time.Second)
+			return safeGet(URL)
+		}
+		log.Fatal(response.StatusCode, err)
 	}
 	return response
 }
@@ -59,10 +67,10 @@ func getFileNameFromPost(post e621Post) string {
 }
 
 func downloadPhoto(path string, URL string) bool {
-	fmt.Println("downloading: " + URL + "\n\tto: " + path)
-
 	webResponse := safeGet(URL)
 	defer webResponse.Body.Close()
+
+	fmt.Println(URL + " ->" + path)
 
 	fileResponse, err := os.Create(path)
 	if err != nil {
@@ -80,34 +88,74 @@ func downloadPhoto(path string, URL string) bool {
 	return true
 }
 
-func downloadE621Page(storagePath string, userName string, limit int, page int) int {
+func downloadE621Page(userName string, limit int, page int) e621PostList {
 	params := map[string]string{"tags": "fav:" + userName, "limit": strconv.Itoa(limit), "page": strconv.Itoa(page)}
 	URL := addParamToURL(homeURL, params)
 	fmt.Println(URL)
 
-	posts := e621PostList{}
-
 	response := safeGet(URL)
 	defer response.Body.Close()
 
+	posts := e621PostList{}
 	json.NewDecoder(response.Body).Decode(&posts)
+	return posts
+}
 
-	fmt.Println(posts.Posts)
+func downloadNewPhotosFromE621PageList(storagePath string, posts e621PostList, existingPostIDs map[int]bool) {
+	for i := 0; i < len(posts.Posts); i++ {
+		if _, ok := existingPostIDs[posts.Posts[i].ID]; ok {
+			log.Println(strconv.Itoa(posts.Posts[i].ID) + " exist, skipping download for this PID")
+			removeFromE621PageList(&posts, i)
+			i--
+		}
+	}
+	downloadAllPhotosFromE621PageList(storagePath, posts)
+}
 
+func downloadAllPhotosFromE621PageList(storagePath string, posts e621PostList) {
 	for _, p := range posts.Posts {
-		<-e621RateLimiter
 		if p.File.URL == "" {
 			go handleMissingURLWithPostID(p.ID)
 			continue
 		}
 		go downloadPhoto(storagePath+"/"+getFileNameFromPost(p), p.File.URL)
 	}
+}
 
-	return len(posts.Posts)
+func removeFromE621PageList(posts *e621PostList, i int) {
+	posts.Posts[i] = posts.Posts[len(posts.Posts)-1]
+	posts.Posts = posts.Posts[:len(posts.Posts)-1]
 }
 
 func handleMissingURLWithPostID(pid int) {
 	missingPID = append(missingPID, pid)
+}
+
+func getExistingPIDInStorage(storagePath string) map[int]bool {
+	exsistingPID := map[int]bool{}
+	err := filepath.Walk(
+		storagePath,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				log.Fatal(err)
+			}
+			PID, err := strconv.Atoi(removeFileNameExt(info.Name()))
+			if err != nil {
+				return nil
+			}
+			exsistingPID[PID] = true
+			return nil
+		},
+	)
+	if err != nil {
+		log.Println(err)
+	}
+	return exsistingPID
+}
+
+func removeFileNameExt(name string) string {
+	splitString := strings.Split(name, ".")
+	return strings.Join(splitString[:len(splitString)-1], ".")
 }
 
 func main() {
@@ -115,17 +163,24 @@ func main() {
 	var storagePath = "/Users/eiffelwong1/Desktop/e621"
 	var postLimit = 320
 
-	var page int = 0
+	var pageNum int = 0
 
 	fmt.Println("start")
+	existingPostIDs := getExistingPIDInStorage(storagePath)
+	print(existingPostIDs)
 
 	for {
-		<-e621RateLimiter
-		downloadCount := downloadE621Page(storagePath, userName, postLimit, page)
-		if downloadCount < postLimit {
+		e621Page := downloadE621Page(userName, postLimit, pageNum)
+		pageNum++
+		go downloadNewPhotosFromE621PageList(storagePath, e621Page, existingPostIDs)
+
+		if len(e621Page.Posts) < postLimit {
 			break
 		}
+
 	}
+
+	fmt.Println("done")
 
 	return
 }
